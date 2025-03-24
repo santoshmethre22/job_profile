77831a9240cceeb7feb128ea7e5a67cc0df1cb99
santosh methre <santoshmethre22@gmail.com>
2025-03-24 17:25:04 +0530

    recruiter model is add

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,1 +1,3 @@
-import { User } from "../models/user.models.js";
+import { User } from "../models/User.models.js";
+
+
---
+++ backend/models/Recuiter.model.js
@@ -0,0 +1,44 @@
+import mongoose from "mongoose";
+
+const recruiterSchema=mongoose.Schema({
+
+    userId:{
+        type:mongoose.Schema.Types.ObjectId,
+        ref:"User"
+
+    },
+
+    name:{
+        type:String,
+        require:true,
+
+    }
+    ,
+
+    company:{
+
+        type:String,
+        require:true
+
+
+    },
+
+    bio:{
+
+        type:String,
+
+    }
+
+
+
+
+},
+{
+
+    timestamps: true,
+    collation: { locale: "en" },
+
+})
+
+
+export const Recruiter=mongoose.model("Recruiter",recruiterSchema)
